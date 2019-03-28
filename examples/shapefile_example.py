64d4b0e4ae16745bed59c63c86cc9f82af02e0e5
Xavier Fontes <xfontes42@gmail.com>
2019-03-28 08:13:09 +0000

    changed structure of repo + working on adding the shapefile example

--- examples/shapefile_example.py
+++ examples/shapefile_example.py
@@ -5,6 +5,9 @@ import geopandas as gpd
 
 
 # open a shapefile, this example contemplates Portugal's shapefile at the 3rd administrative zone level
-shape_file_data = gpd.read_file('templates\\_example_shapefile\\PRT_adm3.shp')
+# change 3 to 0, 1 or 2 and check the difference in granularity
+shape_file_data = gpd.read_file('examples\\_example_shapefile\\PRT_adm3.shp')
+
+# TODO: plot area for visual confirmation
 
 set_trace()
