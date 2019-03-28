d52a128ec2422c3aefe83206a591d8c6c5774ef0
Xavier Fontes <xfontes42@gmail.com>
2019-03-28 08:03:03 +0000

    update to requirements and added excel reading example

---
+++ templates/open_excel.py
@@ -0,0 +1,28 @@
+from ipdb import set_trace          # easy debug
+from pprint import pprint as pp     # pretty print things
+
+import xlrd
+
+
+# Excel file - example with Portugal's population density by administrative zone...
+population_density_excel = xlrd.open_workbook('templates\\_example_pop_density.xlsx')
+# first sheet from Excel file
+population_density_sheet = population_density_excel.sheet_by_index(0)
+
+
+def read_data_into_array(worksheet):
+    """ Helper function to read worksheet into dictionary of values, but could be changed to list """
+    # get header names from worksheet
+    headers = [worksheet.cell_value(0, col) for col in range(worksheet.ncols)]
+    # transform the workbook to a list of dictionary
+    data = [{headers[col]: worksheet.cell_value(row, col)
+             for col in range(worksheet.ncols)
+             } for row in range(1, worksheet.nrows)]
+    return data
+
+
+# dictionary of all values
+density_dictionary = read_data_into_array(population_density_sheet)
+
+set_trace()
+
