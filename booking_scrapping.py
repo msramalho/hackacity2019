545ec23760e6aba2bc555e66d93faa5fb905ecc8
Xavier Fontes <xfontes42@gmail.com>
2019-04-17 09:22:22 +0100

    deleted booking_old, not needed anymore, cleaned scrapping scripts

--- booking_scrapping.py
+++ booking_scrapping.py
@@ -1,90 +1,44 @@
 from ipdb import set_trace, launch_ipdb_on_exception
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
-import time
 import json
-import selenium
 
 
-def scrape_accomodation_data(driver, url):
-    driver.get(url)
-    time.sleep(5)
-
-    acco_fields = dict()
-    acco_fields['name'] = driver.find_element_by_class_name('hp__hotel-name').text.strip('Hotel')
-    acco_fields['address'] = driver.find_element_by_class_name('hp_address_subtitle').get_attribute('data-bbox')
-
-    # WebDriverWait(driver, 1, poll_frequency=0.1)\
-    # .until(ExpectedConditions.presenceOfElementLocated(drv.find_element_by_class_name('bui-review-score__badge')) > 0)
-
-    acco_fields['score'] = dict()
-    if driver.find_elements_by_css_selector('.bui-review-score__badge'):
-        acco_fields['score']['global'] = driver.find_element_by_class_name('bui-review-score__badge').text
-
-        driver.find_element_by_class_name('big_review_score_detailed').click()
-
-        WebDriverWait(driver, 1, poll_frequency=0.1). \
-            until(lambda drv: len(drv.find_elements_by_css_selector('.v2_review-scores__wrapper')) > 0)
-        # if driver.find_elements_by_css_selector('v2_review-scores__wrapper'):
-        for name in driver.find_elements_by_css_selector('.c-score-bar__title'):
-            print(name)
-            acco_fields['score'][name.text] = 'ola'
-
-    else:
-        acco_fields['score']['global'] = -1
-
-    print(acco_fields)
+# url from manually searching Porto in Booking.com and adjusting search parameters (we ran with different params)
+URL_FIRST_PAGE = 'https://www.booking.com/searchresults.pt-pt.html?aid=304142&label=gen173nr-1DCBkoggI46AdIM1gEaLsBiAEBmAEfuAEHyAEM2AED6AEB-AEDiAIBqAIDuAKExZHlBcACAQ&sid=281ee05b273f22e353263ae744fe0913&tmpl=searchresults&class_interval=1&dest_id=-2173088&dest_type=city&from_sf=1&group_adults=2&group_children=0&label_click=undef&nflt=ht_id%3D201%3Bht_id%3D216%3Bht_id%3D220%3Bht_id%3D203%3Bht_id%3D208%3Bht_id%3D222%3B&no_rooms=1&percent_htype_apt=1&raw_dest_type=city&room1=A%2CA&sb_price_type=total&shw_aparth=1&slp_r_match=0&srpvid=7f5c444c6a3600ae&ss=Porto&ssb=empty&ssne=Porto&ssne_untouched=Porto&order=distance_from_search'
 
 
-driver = webdriver.Firefox()
-# driver.get('http://booking.com')
-# driver.find_element_by_css_selector("#ss").send_keys("Porto")
-# WebDriverWait(driver, 1, poll_frequency=0.1).\
-#    until(lambda drv: len(drv.find_elements_by_css_selector(".sb-searchbox__button")) > 0)
-# driver.find_element_by_css_selector(".sb-searchbox__button").click()
+# initiate driver and get the first result page
+ff_driver = webdriver.Firefox()
+ff_driver.get(URL_FIRST_PAGE)
 
-# driver.get(
-#     'https://www.booking.com/searchresults.pt-pt.html?label=gen173nr-1DCBkoggI46AdIM1gEaLsBiAEBmAEfuAEHyAEM2AED6AEB-AEDiAIBqAIDuAKExZHlBcACAQ&sid=2a3e6938484fee9b46b1f05570830152&tmpl=searchresults&class_interval=1&dest_id=-2173088&dest_type=city&dtdisc=0&from_sf=1&group_adults=2&group_children=0&inac=0&index_postcard=0&label_click=undef&no_rooms=1&postcard=0&raw_dest_type=city&room1=A%2CA&sb_price_type=total&shw_aparth=1&slp_r_match=0&src=index&srpvid=d868358750e901e2&ss=Porto&ss_all=0&ssb=empty&sshis=0&ssne=Porto&ssne_untouched=Porto&nflt=ht_id%3D201%3Bht_id%3D216%3Bht_id%3D220%3Bht_id%3D203%3Bht_id%3D208%3Bht_id%3D222%3B&percent_htype_apt=1&rsf=&rows=8000')
-
-driver.get('https://www.booking.com/searchresults.pt-pt.html?aid=304142&label=gen173nr-1DCBkoggI46AdIM1gEaLsBiAEBmAEfuAEHyAEM2AED6AEB-AEDiAIBqAIDuAKExZHlBcACAQ&sid=281ee05b273f22e353263ae744fe0913&tmpl=searchresults&class_interval=1&dest_id=-2173088&dest_type=city&from_sf=1&group_adults=2&group_children=0&label_click=undef&nflt=ht_id%3D201%3Bht_id%3D216%3Bht_id%3D220%3Bht_id%3D203%3Bht_id%3D208%3Bht_id%3D222%3B&no_rooms=1&percent_htype_apt=1&raw_dest_type=city&room1=A%2CA&sb_price_type=total&shw_aparth=1&slp_r_match=0&srpvid=7f5c444c6a3600ae&ss=Porto&ssb=empty&ssne=Porto&ssne_untouched=Porto&order=distance_from_search')
-
-WebDriverWait(driver, 1, poll_frequency=0.1). \
+# wait for page to load
+WebDriverWait(ff_driver, 1, poll_frequency=0.1). \
     until(lambda drv: len(drv.find_elements_by_css_selector("div.sr_item")) > 0)
+
 
 i = 0
 urls = list()
 while True:
 
-    data = list()
-
-    # get the url
-    for link in driver.find_elements_by_css_selector("div.sr_item"):
+    # get the urls in this page and store them
+    for link in ff_driver.find_elements_by_css_selector("div.sr_item"):
         hotel_title = link.find_element_by_class_name("sr-hotel__title")
         urls.append(hotel_title.find_element_by_class_name("hotel_name_link").get_attribute("href"))
 
-    # get the data
-    # for url in urls:
-    # scrape_accomodation_data(driver, url)
-
-    # going to the next page
-
-    # driver.execute_script("window.scrollTo(0, 4500);")
+    # going to the next page, when that fails, store urls collected
     next_page = None
     try:
-        next_page = driver.find_element_by_css_selector(".paging-next")
+        next_page = ff_driver.find_element_by_css_selector(".paging-next")
     except:
         json.dump(urls, open("json_data.json", "a"))
         break
 
+    # repeat for next page
     url = next_page.get_attribute("href")
-
-    driver.get(url)
-
-    WebDriverWait(driver, 1, poll_frequency=1). \
+    ff_driver.get(url)
+    WebDriverWait(ff_driver, 1, poll_frequency=1). \
         until(lambda drv: len(drv.find_elements_by_css_selector("div.sr_item")) > 0)
 
     print(i)
     i += 1
-
-# json.dump(urls, open("json_data.json", "w"))
-# json.load(open())
--- scrape-listing.py
+++ scrape-listing.py
@@ -4,53 +4,56 @@ from selenium.webdriver.support.wait import WebDriverWait
 import time
 import json
 import sys
-import selenium
 import copy
 
+
+# from manually checking the Booking.com website
 SCORE_TYPES = ['staff', 'commodities', 'cleanliness', 'confort', 'price/quality', 'location', 'free-wifi']
 
-def scrape_listing(driver, url, data):
-    driver.get(url)
-    time.sleep(2)
 
+def scrape_listing(driver, url):
+    """From a driver and url fetch important that and save it in a json file."""
+    driver.get(url)
+    time.sleep(2)   # should be better than this but for now it serves its purpose
+
+    # get main fields
     acco_fields = dict()
     acco_fields['name'] = driver.find_element_by_class_name('hp__hotel-name').text.lower()
     acco_fields['address'] = driver.find_element_by_class_name('hp_address_subtitle').text
 
+    # try to get different score metrics
     if driver.find_elements_by_css_selector('.bui-review-score__badge'):
         acco_fields['score_global'] = driver.find_element_by_class_name('bui-review-score__badge').text
-
         driver.find_element_by_class_name('big_review_score_detailed').click()
-
+        # wait for score page to appear
         WebDriverWait(driver, 1, poll_frequency=0.1).\
             until(lambda drv: len(drv.find_elements_by_css_selector('.v2_review-scores__wrapper')) > 0)
-        #if driver.find_elements_by_css_selector('v2_review-scores__wrapper'):
-        for index, name in enumerate(driver.find_elements_by_css_selector('.c-score-bar__score')):
-            acco_fields['score_' + SCORE_TYPES[index]] = name.get_attribute('innerHTML')
+        for index_score, name in enumerate(driver.find_elements_by_css_selector('.c-score-bar__score')):
+            acco_fields['score_' + SCORE_TYPES[index_score]] = name.get_attribute('innerHTML')
 
+    # read licenses and insert each one of them as a unique listing
     licenses = driver.find_element_by_id("hp_important_info_box").find_element_by_tag_name("strong").text.split(",")
     for lic in licenses:
         obj = copy.deepcopy(acco_fields)
         obj['license'] = lic
-        print(obj)
+        # print(obj)
         json.dump(obj, open('booking_new/json_data_%d_%d.json' % (index_start, size), 'a+'))
 
 
 if __name__ == '__main__':
 
+    # gather which indexes to read and load all urls
     index_start, size = list(map(int, sys.argv[1:]))
-
-    data = list()
     urls = json.load(open('json_data.json', 'r'))
 
-    driver = webdriver.Firefox()
-    for index, url in enumerate(urls):
+    ff_driver = webdriver.Firefox()
+    for index, url_to_fetch in enumerate(urls):
         if not (index_start <= index <= index_start + size):
             continue
-
+        # try to scrape listing, failing should only warn the user
         try:
             print('Listing :' + str(index))
-            scrape_listing(driver, url, data)
+            scrape_listing(ff_driver, url_to_fetch)
         except Exception:
-            json.dump(data, open('booking_error/json_data_%d_%d_2.json' % (index_start, size), 'a+'))
+            print("Error parsing %d from json_data_%d_%d" % (index, index_start, size))
 
